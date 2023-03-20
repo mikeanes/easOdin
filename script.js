b416154a6919010dbfab2dbe70d7aab1761aa882
Michael <m.anestopoulos24@gmail.com>
2023-03-20 18:24:52 -0400

    add shading function (doesnt work on rainbow yet)

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ slider.oninput = function(){
 
 //Color Picker
 const colorPicker = document.getElementById('pixelColor');
+    //Rainbow option
+    const rainbow = document.getElementById('rainbow');
+    //Shading option
+    const shading = document.getElementById('shading');
 
 //Function to build grid
 function buildGrid(size){
@@ -25,8 +29,28 @@ function buildGrid(size){
         const div = document.createElement('div');
         div.style.cssText = "border: 1px solid black;";
         div.onmouseenter = function(){
-            div.style.backgroundColor = colorPicker.value;
-            //div.style.cssText = "border: 1px solid black; background-color: black;";
+            let currentColor = div.style.backgroundColor;
+            let shadeValue = parseFloat(div.getAttribute('data-shade')) || 0;
+            if(rainbow.checked){
+                const r = Math.floor(Math.random() * 256);
+                const g = Math.floor(Math.random() * 256);
+                const b = Math.floor(Math.random() * 256);
+                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+                shadeValue = 0;
+            
+            }else if(shading.checked){
+                if(shadeValue < 1){
+                    shadeValue += 0.1;
+                }
+                if(div.style.cssText != "border: 1px solid black; background-color: rgb(0, 0, 0);"){
+                div.style.backgroundColor = `rgb(${(1 - shadeValue) * 255}, ${(1 - shadeValue) * 255}, ${(1 - shadeValue) * 255})`;
+                }
+            }
+            else{
+                div.style.backgroundColor = colorPicker.value;
+                shadeValue = 0;
+            }
+            div.setAttribute('data-shade', shadeValue);
         };
         cdiv.appendChild(div);
     }
