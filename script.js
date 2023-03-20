64dee7245b107cf10fc65df3e1315267365af748
Michael <m.anestopoulos24@gmail.com>
2023-03-20 17:18:38 -0400

    add color change option (functional)

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ slider.oninput = function(){
     currentSize.innerHTML = "Current Size: " + size + " x " + size
 }
 
+//Color Picker
+const colorPicker = document.getElementById('pixelColor');
+
+//Function to build grid
 function buildGrid(size){
     cdiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     cdiv.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -21,23 +25,13 @@ function buildGrid(size){
         const div = document.createElement('div');
         div.style.cssText = "border: 1px solid black;";
         div.onmouseenter = function(){
-            div.classList.add('pixel');
+            div.style.backgroundColor = colorPicker.value;
+            //div.style.cssText = "border: 1px solid black; background-color: black;";
         };
         cdiv.appendChild(div);
     }
 }
 
+
 buildGrid(size);
 
-
-//Color Picker
-let pixelColor;
-const defaultColor = "#ffffff";
-
-window.addEventListener("load", startup, false);
-
-function startup(){
-    pixelColor = document.getElementById('pixelColor');
-    pixelColor.value = defaultColor;
-    
-}
