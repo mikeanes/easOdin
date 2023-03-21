8c9970293a68a9fd26624dbef5d25c047decf9eb
Michael <m.anestopoulos24@gmail.com>
2023-03-20 20:28:32 -0400

    make shading fully functional

--- script.js
+++ script.js
@@ -28,30 +28,23 @@ function buildGrid(size){
     for (let i = 1; i <= (size * size); i++){
         const div = document.createElement('div');
         div.style.cssText = "border: 1px solid black; background-color: white;";
+        let shadeValue = 100;
         div.onmouseenter = function(){
             let currentColor = div.style.backgroundColor;
-            //Use filter start with brightness 100% for everything and remove 10% each time
-            let shadeValue = 100;
             if(rainbow.checked){
                 const r = Math.floor(Math.random() * 256);
                 const g = Math.floor(Math.random() * 256);
                 const b = Math.floor(Math.random() * 256);
-                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
                 shadeValue = 100;
+                div.style.cssText = `border: 1px solid black; background-color: rgb(${r}, ${g}, ${b}); filter: brightness(${shadeValue}%)`;
                 
             }else if(shading.checked){
                 shadeValue -= 10;
                 div.style.cssText = `border: 1px solid black; background-color: ${currentColor}; filter: brightness(${shadeValue}%)`;
-                
-                
-                
-            
             }else{
-                div.style.backgroundColor = colorPicker.value;
                 shadeValue = 100;
+                div.style.cssText = `border: 1px solid black; background-color: ${colorPicker.value}; filter: brightness(${shadeValue}%)`;
             }
-           
-            
         };
         cdiv.appendChild(div);
     }
